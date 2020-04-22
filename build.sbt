7fb19da43ce0b722c893767f05ae4fe00c4ff9e5
Paul Komkoff <i@stingr.net>
2020-04-23 00:15:23 +0100

    SookovnyaBot v1

--- build.sbt
+++ build.sbt
@@ -2,10 +2,14 @@ name := "bototest"
 
 version := "0.1"
 
-scalaVersion := "2.13.1"
+scalaVersion := "2.12.11"
 
 enablePlugins(JavaAppPackaging)
 
-libraryDependencies ++= (
-  "com.bot4s" %% "telegram-core" % "4.4.0-RC2"
+val bot4s = "4.4.0-RC2"
+
+libraryDependencies ++= Seq(
+  "com.bot4s" %% "telegram-core" % bot4s,
+  "com.bot4s" %% "telegram-akka" % bot4s,
+  "com.github.pureconfig" %% "pureconfig" % "0.12.3"
 )
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,61 @@
+import cats.instances.future._
+import cats.syntax.functor._
+import com.softwaremill.sttp._
+import com.bot4s.telegram.future.Polling
+import com.bot4s.telegram.api.declarative.Commands
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+
+case class Body(message: String)
+
+import com.bot4s.telegram.api.AkkaTelegramBot
+import com.bot4s.telegram.clients.AkkaHttpClient
+import slogging.{LogLevel, LoggerConfig, PrintLoggerFactory}
+
+abstract class AkkaExampleBot(val token: String) extends AkkaTelegramBot {
+  LoggerConfig.factory = PrintLoggerFactory()
+  // set log level, e.g. to TRACE
+  LoggerConfig.level = LogLevel.TRACE
+
+  override val client = new AkkaHttpClient(token)
+}
+
+class SokovnyaBot(token: String) extends AkkaExampleBot(token)
+  with Polling
+  with Commands[Future] {
+
+  onCommand("/start") { implicit msg =>
+    reply("Make texting great again!\nUse /covfefe to get a Trump quote.").void
+  }
+
+  onMessage { implicit msg =>
+    using(_.newChatMembers) { newChatMembers =>
+      for (user <- newChatMembers) {
+        val suffix = user.lastName.map(x => s" $x").getOrElse("")
+        reply(s"Здравствуйте ${user.firstName}${suffix}, при входе надо представиться и рассказать о себе. Соковня")
+      }
+      Future.unit
+    }
+  }
+}
+
+case class ServiceConfig(token: String)
+
+import pureconfig._
+import pureconfig.generic.auto._
+
+object SokovnyaMain extends App {
+  ConfigSource.default.load[ServiceConfig].foreach { config =>
+    val bot = new SokovnyaBot(config.token)
+    val eol = bot.run()
+    println("Press [ENTER] to shutdown the bot, it may take a few seconds...")
+    scala.io.StdIn.readLine()
+    println("Calling shutdown.")
+    bot.shutdown() // initiate shutdown
+    // Wait for the bot end-of-life
+    println("Waiting for shut down.")
+    Await.result(eol, Duration.Inf)
+    println("Exiting.")
+  }
+}
